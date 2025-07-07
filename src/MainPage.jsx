0675089219b6bcb09ad54d26f04933c20bd35dd1
psum <sum.philipp@gmail.com>
2025-07-07 21:22:12 +0200

    initial commit

---
+++ src/MainPage.jsx
@@ -0,0 +1,15 @@
+import './Main.css'
+
+export const MainPage = () => {
+	return (
+		<div className='flex justify-center'>
+			<button className='btn btn-primary'>This is a basic button</button>
+			<button className='btn btn-secondary'>This is a basic button</button>
+			<button className='btn btn-accent'>This is a basic button</button>
+			<button className='btn btn-ghost'>This is a basic button</button>
+			<button className='btn btn-link'>This is a basic button</button>
+			<button className='btn btn-outline'>This is a basic button</button>
+			<button className='btn btn-outline'>This is a basic button</button>
+		</div>
+	)
+}
